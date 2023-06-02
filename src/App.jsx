8c55a9ac50465df796e319b8efac3eb041cd69b0
hsivritepe <hakan@sivritepe.com>
2023-06-02 13:46:15 -0700

    New funcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
+import './App.scss';
+import Mapbox from './components/Map/Mapbox';
 
-import "./App.scss";
-import Mapbox from "./components/Map/Mapbox";
+import HomePage from './components/HomePage';
+import GamePage from './components/GamePage';
 
-import HomePage from "./components/HomePage";
-import GamePage from "./components/GamePage";
+import {
+    getGeoLocationFromAPI,
+    updateUsersScore,
+    getUsersForScoreboardOrderedForScore,
+    registerUser,
+} from './utils';
 
-import getGeoLocationFromAPI from './utils'
-
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    Navigate,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/game/questions" element={<Mapbox />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    // // Examples to use the finctions
+    // registerUser('testqewrqw');
+    // getUsersForScoreboardOrderedForScore();
+    // updateUsersScore('bde3e1f9-1f21-4da1-80e4-72345bd22ae8', {
+    //     name: 'testqewrqw',
+    //     score: 100,
+    // });
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route
+                        path="/home"
+                        element={<Navigate to="/" />}
+                    />
+                    <Route path="/game" element={<GamePage />} />
+                    <Route
+                        path="/game/questions"
+                        element={<Mapbox />}
+                    />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
