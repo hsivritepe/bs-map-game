46f32fba559d5650618abb1d78bd25afab3e4ad8
Nick Seale <NickSeale>
2023-06-02 14:25:33 -0700

    Two marker functionality

--- src/components/Map/Mapbox.jsx
+++ src/components/Map/Mapbox.jsx
@@ -1,97 +1,97 @@
-import "./Map.scss";
-import Map, { Marker } from "react-map-gl";
-import { useState } from "react";
-import "mapbox-gl/dist/mapbox-gl.css";
-// import { tsCallSignatureDeclaration } from "@babel/types";
+// import "./Map.scss";
+// import Map, { Marker } from "react-map-gl";
+// import { useState } from "react";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// // import { tsCallSignatureDeclaration } from "@babel/types";
 
-function Mapbox(props) {
-  let cityLocation = "";
-  // props.getGeoLocationFromAPI("ottawa", (cityData) => {
-  //   cityLocation = cityData;
-  // });
-  console.log(cityLocation);
-  // const [viewport, setViewport] = useState({
-  //   latitude: 45.4211,
-  //   longitude: -75.6903,
-  //   zoom: 2,
-  // });
+// function Mapbox(props) {
+//   let cityLocation = "";
+//   // props.getGeoLocationFromAPI("ottawa", (cityData) => {
+//   //   cityLocation = cityData;
+//   // });
+//   console.log(cityLocation);
+//   // const [viewport, setViewport] = useState({
+//   //   latitude: 45.4211,
+//   //   longitude: -75.6903,
+//   //   zoom: 2,
+//   // });
 
-  // PROPS WILL SEND ME LAT AND LONG
-  // FOR NOW WE JUST IMAGINE THAT LAT LONG IS A NUMBER
-  // SOON IT WILL BE GIVEN TO US
+//   // PROPS WILL SEND ME LAT AND LONG
+//   // FOR NOW WE JUST IMAGINE THAT LAT LONG IS A NUMBER
+//   // SOON IT WILL BE GIVEN TO US
 
-  // let cityLat = props.cityLat
-  // let cityLng = props.cityLng
-  // let showUserMarker = props.showUserMarker;
-  // let showAnswerMarker = props.showAnswerMarker;
-  // let userScore = props.userScore;
+//   // let cityLat = props.cityLat
+//   // let cityLng = props.cityLng
+//   let showUserMarker = props.showUserMarker;
+//   let showAnswerMarker = props.showAnswerMarker;
+//   // let userScore = props.userScore;
 
-  // TEMPORARY
-  const cityLat = 45.4211;
-  const cityLng = -75.6903;
-  let showUserMarker = true;
-  let showAnswerMarker = true;
-  let userScore = 0;
+//   // TEMPORARY
+//   const cityLat = 45.4211;
+//   const cityLng = -75.6903;
+//   // let showUserMarker = true;
+//   // let showAnswerMarker = true;
+//   let userScore = 0;
 
-  const [markerLocation, setMarkerLocation] = useState([]);
+//   const [markerLocation, setMarkerLocation] = useState([]);
 
-  console.log("trying to load map");
+//   console.log("trying to load map");
 
-  const handleUserClick = (event) => {
-    const clickLocation = event.lngLat;
-    console.log("user clicked at ", clickLocation);
-    setMarkerLocation([
-      { latitude: clickLocation.lat, longitude: clickLocation.lng },
-    ]);
-    calculateUserScore(clickLocation);
-  };
+//   const handleUserClick = (event) => {
+//     const clickLocation = event.lngLat;
+//     console.log("user clicked at ", clickLocation);
+//     setMarkerLocation([
+//       { latitude: clickLocation.lat, longitude: clickLocation.lng },
+//     ]);
+//     calculateUserScore(clickLocation);
+//   };
 
-  const calculateUserScore = (clickLocation) => {
-    const latDif = clickLocation.lat - cityLat;
-    const lngDif = clickLocation.lng - cityLng;
+//   const calculateUserScore = (clickLocation) => {
+//     const latDif = clickLocation.lat - cityLat;
+//     const lngDif = clickLocation.lng - cityLng;
 
-    const score = Math.sqrt(Math.pow(latDif, 2) + Math.pow(lngDif, 2)).toFixed(
-      2
-    );
-    userScore += score;
-    console.log("user score is", score);
+//     const score = Math.sqrt(Math.pow(latDif, 2) + Math.pow(lngDif, 2)).toFixed(
+//       2
+//     );
+//     userScore += score;
+//     console.log("user score is", score);
 
-    if (showUserMarker === false) {
-      setMarkerLocation([]);
-    }
-    // THIS IS THE SEND SCORE FOR LATER
-    // props.sendScore(score);
-  };
+//     if (showUserMarker === false) {
+//       setMarkerLocation([]);
+//     }
+//     // THIS IS THE SEND SCORE FOR LATER
+//     // props.sendScore(score);
+//   };
 
-  return (
-    <div className="mapbox">
-      <Map
-        initialViewState={{ latitude: 45.4211, longitude: -75.6903, zoom: 2 }}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/ns79765/cliew2o1d002o01odb2hhfnzp"
-        style={{ width: "100vw", height: "100vh" }}
-        projection="globe"
-        onClick={(event) => {
-          handleUserClick(event);
-        }}
-      >
-        {markerLocation.map((element, index) => (
-          <>
-            {/* USER MARKER */}
-            <Marker
-              latitude={element.latitude}
-              longitude={element.longitude}
-              key={index}
-            />
-          </>
-        ))}
-        {/* ANSWER MARKER */}
-        {showAnswerMarker && (
-          <Marker latitude={cityLat} longitude={cityLng} key={"answer"} />
-        )}
-      </Map>
-    </div>
-  );
-}
+//   return (
+//     <div className="mapbox">
+//       <Map
+//         initialViewState={{ latitude: 45.4211, longitude: -75.6903, zoom: 2 }}
+//         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+//         mapStyle="mapbox://styles/ns79765/cliew2o1d002o01odb2hhfnzp"
+//         style={{ width: "100vw", height: "100vh" }}
+//         projection="globe"
+//         onClick={(event) => {
+//           handleUserClick(event);
+//         }}
+//       >
+//         {markerLocation.map((element, index) => (
+//           <>
+//             {/* USER MARKER */}
+//             <Marker
+//               latitude={element.latitude}
+//               longitude={element.longitude}
+//               key={index}
+//             />
+//           </>
+//         ))}
+//         {/* ANSWER MARKER */}
+//         {showAnswerMarker && (
+//           <Marker latitude={cityLat} longitude={cityLng} key={"answer"} />
+//         )}
+//       </Map>
+//     </div>
+//   );
+// }
 
-export default Mapbox;
+// export default Mapbox;
--- src/components/QuestionPage.jsx
+++ src/components/QuestionPage.jsx
@@ -1,16 +1,54 @@
-import MapBox from "./Map/Mapbox";
+import { useState } from "react";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function QuestionPage() {
+  console.log("trying to load map");
+
+  // TEMPORARY
   const currentCity = "New York";
   const currentScore = 500;
   const userName = "Michael";
+  const cityLat = 45.4211;
+  const cityLng = -75.6903;
+  let userScore = 0;
+
+  const [showUserMarker, setShowUserMarker] = useState(true);
+  const [showAnswerMarker, setShowAnswerMarker] = useState(false);
+  const [markerLocation, setMarkerLocation] = useState([]);
 
   const handleNext = () => {
-    //TODO
+    console.log("next button clicked");
+    setShowAnswerMarker(false);
+    setShowUserMarker(false);
   };
 
   const handleSubmit = () => {
-    //TODO
+    console.log("submit button clicked");
+    setShowAnswerMarker(true);
+  };
+
+  const handleUserClick = (event) => {
+    const clickLocation = event.lngLat;
+    setShowUserMarker(true);
+    console.log("user clicked at ", clickLocation);
+    setMarkerLocation([
+      { latitude: clickLocation.lat, longitude: clickLocation.lng },
+    ]);
+    calculateUserScore(clickLocation);
+  };
+
+  const calculateUserScore = (clickLocation) => {
+    const latDif = clickLocation.lat - cityLat;
+    const lngDif = clickLocation.lng - cityLng;
+
+    const score = Math.sqrt(Math.pow(latDif, 2) + Math.pow(lngDif, 2)).toFixed(
+      2
+    );
+    userScore += score;
+    console.log("user score is", score);
+    // THIS IS THE SEND SCORE FOR LATER
+    // props.sendScore(score);
   };
 
   return (
@@ -18,15 +56,39 @@ function QuestionPage() {
       <div className="left-side">
         <h2>Current City:</h2>
         <h1>{currentCity}</h1>
-
         <p>User Name: {userName}</p>
         <p>Current Score: {currentScore}</p>
         <button onClick={handleSubmit}>Submit</button>
-
         <button onClick={handleNext}>Next</button>
       </div>
 
-      <MapBox />
+      <div className="mapbox">
+        <Map
+          initialViewState={{ latitude: 45.4211, longitude: -75.6903, zoom: 2 }}
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/ns79765/cliew2o1d002o01odb2hhfnzp"
+          style={{ width: "100vw", height: "100vh" }}
+          projection="globe"
+          onClick={(event) => {
+            handleUserClick(event);
+          }}
+        >
+          {/* USER MARKER */}
+          {showUserMarker &&
+            markerLocation.map((element, index) => (
+              <Marker
+                latitude={element.latitude}
+                longitude={element.longitude}
+                key={index}
+              />
+            ))}
+
+          {/* ANSWER MARKER */}
+          {showAnswerMarker && (
+            <Marker latitude={cityLat} longitude={cityLng} key={"answer"} />
+          )}
+        </Map>
+      </div>
     </div>
   );
 }
