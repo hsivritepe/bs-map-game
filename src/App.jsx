a403fb0d4c552c11e2b30cdebbe249966dd25fc6
xieyutia <sunnyyutian@outlook.com>
2023-06-02 11:42:48 -0700

    homepage, gamepage, questionpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
+import HomePage from "./components/HomePage";
+import GamePage from "./components/GamePage";
+
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
