aa553d69674aa9b00e72914b24e0f3f035bc6017
Nick Seale <NickSeale>
2023-06-02 13:49:20 -0700

    Fix merge conflicts with main branch

--- src/components/Map/Mapbox.jsx
+++ src/components/Map/Mapbox.jsx
@@ -6,9 +6,9 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 function Mapbox(props) {
   let cityLocation = "";
-  props.getGeoLocationFromAPI("ottawa", (cityData) => {
-    cityLocation = cityData;
-  });
+  // props.getGeoLocationFromAPI("ottawa", (cityData) => {
+  //   cityLocation = cityData;
+  // });
   console.log(cityLocation);
   // const [viewport, setViewport] = useState({
   //   latitude: 45.4211,
